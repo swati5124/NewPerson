768f71120427059050bfd2b70ebb051d47552871
swati5124 <swati@niit-tech.com>
2020-04-03 14:08:27 +0530

    InitialCommit

---
+++ NewgitBranch3april/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace NewgitBranch3april.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
